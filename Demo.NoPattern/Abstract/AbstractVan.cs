007879f6d6bd673d0f6fdad0a171f8057bdaa550
Andrea Merlin <merlin.andrea@gmail.com>
2018-03-06 22:45:23 +0100

    add AbstractVan class

---
+++ Demo.NoPattern/Abstract/AbstractVan.cs
@@ -0,0 +1,16 @@
+﻿using Demo.NoPattern.Enum;
+using Demo.NoPattern.Interface;
+
+namespace Demo.NoPattern.Abstract
+{
+    public abstract class AbstractVan: AbstractCar
+    {
+        protected AbstractVan(IEngine engine) : base(engine)
+        {
+        }
+
+        protected AbstractVan(IEngine engine, VehicleColour colour) : base(engine, colour)
+        {
+        }
+    }
+}
