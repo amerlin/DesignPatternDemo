abec90fb3835b21412b8f0b551d7f2ab74eb3977
Andrea Merlin <merlin.andrea@gmail.com>
2018-03-06 22:36:47 +0100

    add Standard engine class add TurboEngine class

---
+++ Demo.NoPattern/Concrete/TurboEngine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.NoPattern.Abstract;
+
+namespace Demo.NoPattern.Concrete
+{
+    public class TurboEngine: AbstractEngine
+    {
+        public TurboEngine(int size) : base(size, true)
+        {
+            //turbo
+        }
+    }
+}
