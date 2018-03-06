4d5b8b56d7b7c722065fd95537f624c812a921d2
Andrea Merlin <merlin.andrea@gmail.com>
2018-03-06 22:43:05 +0100

    add abstract vehicle

---
+++ Demo.NoPattern/Abstract/AbstractVehicle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.NoPattern.Enum;
+using Demo.NoPattern.Interface;
+
+namespace Demo.NoPattern.Abstract
+{
+    public abstract class AbstractVehicle: IVehicle
+    {
+        private IEngine engine;
+        private VehicleColour colour;
+
+        public AbstractVehicle(IEngine engine): this(engine, VehicleColour.Unpainted)
+        {
+            this.engine = engine;
+        }
+
+        public AbstractVehicle(IEngine engine, VehicleColour colour)
+        {
+            this.engine = engine;
+            this.colour = colour;
+        }
+
+        public virtual IEngine Engine
+        {
+            get { return engine; }
+        }
+
+        public virtual VehicleColour Colour
+        {
+            get { return colour; }
+        }
+
+
+        public void Paint(VehicleColour colour)
+        {
+            this.colour = colour;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.GetType().Name} ({engine}/{colour})";
+        }
+
+
+    }
+}
