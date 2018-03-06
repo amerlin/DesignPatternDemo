2980e03a806d1c99f86a81f583122a014312e963
Andrea Merlin <merlin.andrea@gmail.com>
2018-03-06 22:44:27 +0100

    add abstractCar class

---
+++ Demo.NoPattern/Abstract/AbstractCar.cs
@@ -0,0 +1,16 @@
+﻿using Demo.NoPattern.Enum;
+using Demo.NoPattern.Interface;
+
+namespace Demo.NoPattern.Abstract
+{
+    public abstract class AbstractCar: AbstractVehicle
+    {
+        protected AbstractCar(IEngine engine) : base(engine)
+        {
+        }
+
+        protected AbstractCar(IEngine engine, VehicleColour colour) : base(engine, colour)
+        {
+        }
+    }
+}
