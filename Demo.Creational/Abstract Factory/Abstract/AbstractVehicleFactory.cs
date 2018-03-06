42b2040d8bf4f50c55cc6bc69deac653bda739ff
Andrea Merlin <merlin.andrea@gmail.com>
2018-03-06 23:08:24 +0100

    create abstractvehiclefactory

---
+++ Demo.Creational/Abstract Factory/Abstract/AbstractVehicleFactory.cs
@@ -0,0 +1,11 @@
+﻿using Demo.Creational.Abstract_Factory.Interface;
+
+namespace Demo.Creational.Abstract_Factory.Abstract
+{
+    public abstract class AbstractVehicleFactory
+    {
+        public abstract IBody CreateBody();
+        public abstract IChassis CreateChassis();
+        public abstract IGlassware CreateGlassware();
+    }
+}
