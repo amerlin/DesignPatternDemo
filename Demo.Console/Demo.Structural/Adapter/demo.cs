5590218b0cacc506c02719e32ca10a88e2155df5
Andrea Merlin <merlin.andrea@gmail.com>
2018-03-20 23:02:15 +0100

    add demo comment

--- Demo.Console/Demo.Structural/Adapter/demo.cs
+++ Demo.Console/Demo.Structural/Adapter/demo.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Demo.Structural.Adapter.Adapter;
 using Demo.Structural.Adapter.Concrete;
 using Demo.Structural.Adapter.Interface;
@@ -11,6 +7,7 @@ namespace Demo.Structural.Adapter
 {
     public class Demo
     {
+        //Demo Adapter
         public Demo()
         {
             //normal 
