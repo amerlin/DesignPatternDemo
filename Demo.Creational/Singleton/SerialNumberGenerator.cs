7710b199d482b69bd4ad6d68bcfa0a3c52ea5310
Andrea Merlin <merlin.andrea@gmail.com>
2018-03-20 20:40:20 +0100

    singleton code refactoring

--- Demo.Creational/Singleton/SerialNumberGenerator.cs
+++ Demo.Creational/Singleton/SerialNumberGenerator.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Demo.Creational.Singleton
+﻿namespace Demo.Creational.Singleton
 {
     public class SerialNumberGenerator
     {
